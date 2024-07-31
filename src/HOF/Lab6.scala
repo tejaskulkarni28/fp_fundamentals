01fddba20b5a6a65987789ab4b7b150fdbdfacf7
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-07-31 18:43:27 +0530

    HOF practice :)

--- src/HOF/Lab6.scala
+++ src/HOF/Lab6.scala
@@ -2,7 +2,11 @@ package HOF
 
 object Lab6 extends App {
   // practicing hof -> return type is a function
-  val loop = (f: Int=>Int, n: Int)=>(x:Int=>Int)=>{
-
+  private val loop:(Int=>Int,Int)=>(Int=>Int)=(f:Int=>Int, n:Int)=>{
+    if(n <= 0) (x:Int)=>x
+    else (x:Int)=>loop(f, n-1)(f(x))
   }
+  private val returnSomeThing = (x: Int)=>x
+  private val output = loop(returnSomeThing, 10)(0)
+  println(output) // will return some value based on the returnSomething function`
 }
