01963c9cd5e1a97f340032544f2ab870f4dff919
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-08-02 22:51:25 +0530

    Hof:

--- src/HOF/Lab5.scala
+++ src/HOF/Lab5.scala
@@ -1,5 +1,7 @@
 package HOF
 
+import scala.util.Random
+
 object Lab5 extends App {
   private val numbers = List(1, 2, 3, 4)
   private val chars = List('a', 'b', 'c', 'd')
@@ -13,4 +15,12 @@ object Lab5 extends App {
     c <- chars
   } yield "" + n + c
   println(concatenate)
+
+  private val names = List("John", "Jack", "Jimmy")
+  private val randomNumbers:Random = Random
+  private val generateNumbersList = List.fill(names.length)(randomNumbers.nextInt(100))
+  private val concatenateList = for{
+    (name, randomNumber) <- names zip generateNumbersList
+  }yield s"$name$randomNumber"
+  println(concatenateList)
 }
