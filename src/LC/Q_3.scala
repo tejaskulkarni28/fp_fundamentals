7b195219940a4cbc440e4a521a02c07a59cb84a5
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-08-21 23:32:04 +0530

    Trying to catch some dsa

--- src/LC/Q_3.scala
+++ src/LC/Q_3.scala
@@ -1,4 +1,25 @@
 package LC
 
 object Q_3 extends App {
+
+  // abcabcbb <- testcase
+  println(longestSubString("abcabcbb"))
+  def longestSubString(s: String):Int= {
+    // initialization
+    var maxLength = 0
+    var startWindow = 0
+    var map = scala.collection.mutable.Map[Char, Int]()
+
+    for (endWindow <- s.indices){
+      val currentCharacter = s(endWindow)
+
+      if(map.contains(currentCharacter)){
+        startWindow = map(currentCharacter) + 1
+      }
+
+      map.put(currentCharacter, endWindow) // updating
+      maxLength = Math.max(maxLength, endWindow - startWindow + 1)
+    }
+    maxLength
+  }
 }
